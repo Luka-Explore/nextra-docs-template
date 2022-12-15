9a4d077639a2b892ec8b895da1e12982d1471408
Luka Beverin <luka@explore-datascience.net>
2022-12-15 11:26:08 +0100

    add project

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: () => (
+    <>
+      <img
+        src="/logo.jpg"
+        height="120"
+        width="120"
+        style={{ marginRight: "1em" }}
+      />
+      <h1>
+        Internship Workshop <span style={{ opacity: 0.2 }}></span>
+      </h1>
+    </>
+  ),
+
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
+  docsRepositoryBase: "https://vercel.com/templates/next.js/documentation-starter-kit",
+/*
+  editLink: { 
+    component: <a href="https://nextra.site">
+  </a>,
   },
+  */
+  footer: {
+    text: ' © ExploreAI Academy',
+  },
+  darkMode: false,
 }
 
+
 export default config
+
